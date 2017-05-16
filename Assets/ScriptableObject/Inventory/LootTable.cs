7c6f086131834f5dcdd1ad3d9f3e20cef3b11724
Benjamin Odom <bennybroseph@gmail.com>
2017-05-16 15:47:43 -0500

    Loot Table
    Added a scriptable object which defines a loot table. Has the ability to
    test which items may drop on a given execution.

---
+++ Assets/ScriptableObject/Inventory/LootTable.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
+using UnityEngine;
+
+using Random = UnityEngine.Random;
+
+[CreateAssetMenu]
+public class LootTable : ScriptableObject
+{
+    [Serializable]
+    public class ItemDrop
+    {
+        public Item item;
+
+        [Range(0f, 100f)]
+        public float dropChance = 100f;
+    }
+
+    public ItemDrop[] itemDrops;
+
+    public IEnumerable<Item> GetDrops()
+    {
+        foreach (var itemDrop in itemDrops)
+        {
+            var roll = Random.Range(0f, 100f);
+            if (roll <= itemDrop.dropChance)
+                yield return itemDrop.item;
+        }
+    }
+
+#if UNITY_EDITOR
+
+    [CustomEditor(typeof(LootTable))]
+    public class EditorLootTable : Editor
+    {
+        private LootTable m_LootTable;
+        private string m_Result;
+
+        private void OnEnable()
+        {
+            m_LootTable = target as LootTable;
+        }
+
+        public override void OnInspectorGUI()
+        {
+            base.OnInspectorGUI();
+
+            EditorGUILayout.Space();
+
+            if (GUILayout.Button("Test Drop"))
+            {
+                if (m_LootTable == null)
+                    return;
+
+                m_Result = string.Empty;
+
+                var itemDrops = m_LootTable.GetDrops();
+                foreach (var itemDrop in itemDrops)
+                    m_Result += itemDrop.ToString() + '\n';
+
+                m_Result = m_Result.TrimEnd('\n');
+
+                if (m_Result == string.Empty)
+                    m_Result = "No Drops";
+            }
+
+            if (m_Result != null)
+                EditorGUILayout.TextArea(m_Result);
+        }
+    }
+
+#endif
+}
